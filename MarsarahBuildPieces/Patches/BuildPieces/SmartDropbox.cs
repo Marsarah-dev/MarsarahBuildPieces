4cb22ae53e214653cf2382c206c284b6fb7e51af
Marius Corca <mariuscorca@gmail.com>
2026-09-24 11:10:12 +0200

    Attempt to add MUC flag to Smart Dropbox.

--- MarsarahBuildPieces/Patches/BuildPieces/SmartDropbox.cs
+++ MarsarahBuildPieces/Patches/BuildPieces/SmartDropbox.cs
@@ -25,6 +25,8 @@ namespace MarsarahBuildPieces.Patches.BuildPieces
 		private static ZRoutedRpc registeredRoutedRpc;
 		private static readonly int SmartDropboxPrefabHash = "smart_dropbox".GetStableHashCode();
 
+		private const string MultiUserChestIgnoreKey = "MUC_Ignore";
+
 		private static readonly HashSet<int> SupportedStoragePrefabs = new HashSet<int>
 		{
 			"piece_chest_wood".GetStableHashCode(),
@@ -41,6 +43,26 @@ namespace MarsarahBuildPieces.Patches.BuildPieces
 			internal int Index;
 			internal int TotalMoved;
 			internal int ContainersUsed;
+		}
+
+		internal static bool ApplyMultiUserChestIgnoreFlag(ZNetView nview)
+		{
+			if (nview == null || !nview.IsValid() || !nview.IsOwner())
+				return false;
+
+			ZDO zdo = nview.GetZDO();
+			if (zdo == null)
+				return false;
+
+			if (!zdo.GetBool(MultiUserChestIgnoreKey))
+			{
+				zdo.Set(MultiUserChestIgnoreKey, true);
+				ZDOMan.instance?.ForceSendZDO(zdo.m_uid);
+
+				log.Info($"Applied MultiUserChest ignore flag | ZDO={zdo.m_uid}");
+			}
+
+			return true;
 		}
 
 		private static readonly Dictionary<ZDOID, DistributionTransaction> activeDistributions = new Dictionary<ZDOID, DistributionTransaction>();
@@ -983,6 +1005,12 @@ namespace MarsarahBuildPieces.Patches.BuildPieces
 			{
 				transaction.TotalMoved += moved;
 				transaction.ContainersUsed++;
+
+				GameObject destinationPrefab = ZNetScene.instance?.GetPrefab(destination.GetPrefab());
+				string prefabName = destinationPrefab != null ? destinationPrefab.name : destination.GetPrefab().ToString();
+				Vector3 position = destination.GetPosition();
+
+				log.Info($"Transferred items | Moved={moved} | Destination={destination.m_uid} | Prefab={prefabName} | Position={position.x:0.0}, {position.y:0.0}, {position.z:0.0}");
 			}
 
 			if (expectedOwner != serverId)
@@ -1179,6 +1207,8 @@ namespace MarsarahBuildPieces.Patches.BuildPieces
 		{
 			nview = SmartDropbox.GetContainerZNetView(GetComponent<Container>());
 
+			StartCoroutine(ApplyMultiUserChestIgnoreFlag());
+
 			SmartDropbox.HideRadiusMarker(gameObject);
 			SmartDropbox.RegisterDropbox(this);
 		}
@@ -1194,6 +1224,19 @@ namespace MarsarahBuildPieces.Patches.BuildPieces
 				nview = SmartDropbox.GetContainerZNetView(GetComponent<Container>());
 
 			SmartDropbox.RequestServerHandoff(nview);
+		}
+
+		private IEnumerator ApplyMultiUserChestIgnoreFlag()
+		{
+			float startTime = Time.realtimeSinceStartup;
+
+			while (Time.realtimeSinceStartup - startTime < 5f)
+			{
+				if (SmartDropbox.ApplyMultiUserChestIgnoreFlag(nview))
+					yield break;
+
+				yield return new WaitForSecondsRealtime(0.1f);
+			}
 		}
 	}
 }
